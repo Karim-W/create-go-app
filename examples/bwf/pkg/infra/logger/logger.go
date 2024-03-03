9ebd05f3dd26ad347b763dd7679d9a5ca18f953a
karim-w <karim.wael@gmail.com>
2024-03-03 14:02:05 +0400

    template: used custom logger for both templates

--- examples/bwf/pkg/infra/logger/logger.go
+++ examples/bwf/pkg/infra/logger/logger.go
@@ -2,14 +2,40 @@ package logger
 
 import (
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 // InitOrDie initializes the logger and panics if it fails.
 func InitOrDie() *zap.Logger {
-	logginInstance, err := zap.NewProduction()
-	if err != nil {
-		panic(err)
+	encoderCfg := zap.NewProductionEncoderConfig()
+
+	encoderCfg.TimeKey = "ts"
+
+	encoderCfg.EncodeTime = zapcore.ISO8601TimeEncoder
+	encoderCfg.EncodeLevel = zapcore.CapitalLevelEncoder
+
+	config := zap.Config{
+		Level:             zap.NewAtomicLevelAt(zap.InfoLevel),
+		Development:       false,
+		DisableCaller:     true,
+		DisableStacktrace: true,
+		Sampling:          nil,
+		Encoding:          "json",
+		EncoderConfig:     encoderCfg,
+		OutputPaths: []string{
+			"stdout",
+		},
+		ErrorOutputPaths: []string{
+			"stderr",
+		},
+		// InitialFields: map[string]interface{}{
+		// 	"pid": os.Getpid(),
+		// },
 	}
+
+	logginInstance := zap.Must(config.Build())
+
 	zap.ReplaceGlobals(logginInstance)
+
 	return logginInstance
 }
